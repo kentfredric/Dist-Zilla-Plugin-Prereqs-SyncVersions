069b8c5c5d243061ee52ec84dd91048ed6dc9e4b
Kent Fredric <kentfredric@gmail.com>
2013-10-12 17:39:46 +1300

    Misc doc/critic fixups

--- lib/Dist/Zilla/Plugin/Prereqs/SyncVersions.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/SyncVersions.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Prereqs::SyncVersions;
 
-# ABSTRACT: Homogenise prerequisites so dependency versions are consistent
+# ABSTRACT: Homogenize prerequisites so dependency versions are consistent
 
 use Moose;
 use MooseX::Types::Moose qw( HashRef ArrayRef Str );
@@ -32,9 +32,9 @@ Note: This must come B<after> packages that add their own prerequisites in order
 
 =head1 DESCRIPTION
 
-This module exists to pose mostly as a workaround for potential bugs in downstream toolchains.
+This module exists to pose mostly as a workaround for potential bugs in downstream tool-chains.
 
-Namely, CPAN.pm is confused when it sees:
+Namely, C<CPAN.pm> is confused when it sees:
 
     runtime.requires : Foo >= 5.0
     test.requires    : Foo >= 6.0
@@ -43,13 +43,34 @@ It doesn't know what to do.
 
 This is an easy enough problem to solve if you're using C<[Prereqs]> directly,
 and C<[AutoPrereqs]> already does the right thing, but it gets messier
-when you're working with L<< plugins that inject their own prereqs|https://github.com/dagolden/Path-Tiny/commit/c620171db96597456a182ea6088a24d8de5debf6 >>
+when you're working with L<< plugins that inject their own prerequisites>|https://github.com/dagolden/Path-Tiny/commit/c620171db96597456a182ea6088a24d8de5debf6 >>
 
-So this plugin will homogenise dependencies to be the same version in all phases
+So this plugin will homogenize dependencies to be the same version in all phases
 which infer the dependency, matching the largest one found, so the above becomes:
 
     runtime.requires : Foo >= 6.0
     test.requires    : Foo >= 6.0
+
+=cut
+
+=attr C<applyto_phase>
+
+A multi-value attribute that specifies which phases to iterate and homogenize.
+
+By default, this is:
+
+    applyto_phase = build
+    applyto_phase = test
+    applyto_phase = runtime
+    applyto_phase = configure
+
+However, you could extend it further to include C<develop> if you wanted to.
+
+    applyto_phase = build
+    applyto_phase = test
+    applyto_phase = runtime
+    applyto_phase = configure
+    appyyto_phase = develop
 
 =cut
 
@@ -60,11 +81,43 @@ has applyto_phase => (
   default => sub { [qw(build test runtime configure)] },
 );
 
+=attr C<applyto_relation>
+
+A multi-value attribute that specifies which relations to iterate and homogenize.
+
+By default, this is:
+
+    applyto_relation = requires
+
+However, you could extend it further to include C<suggests> and C<recommends> if you wanted to.
+You could even add C<conflicts> ... but you really shouldn't.
+
+    applyto_relation = requires
+    applyto_relation = suggests
+    applyto_relation = recommends
+    applyto_relation = conflicts ; Danger will robinson.
+
+=cut
+
 has applyto_relation => (
   is => ro => isa => ArrayRef [Str],
   lazy    => 1,
   default => sub { [qw(requires)] },
 );
+
+=attr C<applyto>
+
+A multi-value attribute that by default composites the values of
+
+C<applyto_relation> and C<applyto_phase>.
+
+This is if you want to be granular about how you specify phase/relations to process.
+
+    applyto = runtime.requires
+    applyto = develop.requires
+    applyto = test.suggests
+
+=cut
 
 has applyto => (
   is => ro =>,
@@ -98,13 +151,15 @@ sub _set_module_version {
   my ( $self, $module, $version ) = @_;
   if ( not exists $self->_max_versions->{$module} ) {
     $self->_max_versions->{$module} = $self->_versionify($version);
+    return;
   }
   my $comparator = $self->_versionify($version);
   my $current    = $self->_max_versions->{$module};
   if ( $current < $comparator ) {
-    $self->log_debug( "Version upgrade on : " . $module );
+    $self->log_debug( [ 'Version upgrade on : %s', $module ] );
     $self->_max_versions->{$module} = $comparator;
   }
+  return;
 }
 
 sub _get_module_version {
@@ -136,9 +191,17 @@ sub _build__applyto_list {
   return \@out;
 }
 
-sub mvp_multivalue_args { return qw( applyto applyto_relation applyto_phase ) }
+=method C<mvp_multivalue_args>
 
-sub mvp_aliases { return { 'module' => 'modules' } }
+The following attributes exist, and may be specified more than once:
+
+    applyto
+    applyto_relation
+    applyto_phase
+
+=cut
+
+sub mvp_multivalue_args { return qw( applyto applyto_relation applyto_phase ) }
 
 around dump_config => sub {
   my ( $orig, $self ) = @_;
@@ -152,7 +215,7 @@ around dump_config => sub {
   return $config;
 };
 
-sub foreach_phase_rel {
+sub _foreach_phase_rel {
   my ( $self, $prereqs, $callback ) = @_;
   for my $applyto ( @{ $self->_applyto_list } ) {
     my ( $phase, $rel ) = @{$applyto};
@@ -163,13 +226,20 @@ sub foreach_phase_rel {
   return;
 }
 
+=method C<register_prereqs>
+
+This method is called during C<Dist::Zilla> prerequisite generation,
+and it injects supplementary prerequisites to make things match up.
+
+=cut
+
 sub register_prereqs {
   my ($self)  = @_;
   my $zilla   = $self->zilla;
   my $prereqs = $zilla->prereqs;
   my $guts = $prereqs->cpan_meta_prereqs->{prereqs} || {};
 
-  $self->foreach_phase_rel(
+  $self->_foreach_phase_rel(
     $guts => sub {
       my ( $phase, $rel, $reqs ) = @_;
       for my $module ( keys %{$reqs} ) {
@@ -177,7 +247,7 @@ sub register_prereqs {
       }
     }
   );
-  $self->foreach_phase_rel(
+  $self->_foreach_phase_rel(
     $guts => sub {
       my ( $phase, $rel, $reqs ) = @_;
       for my $module ( keys %{$reqs} ) {
